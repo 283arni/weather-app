f0eb782ebda9e6ae0f512798ecb6e260813693d1
Aleksandr Arnautov <283arni@gmail.com>
2020-08-21 21:26:48 +0300

    fix

--- src/components/details-main/details-main.jsx
+++ src/components/details-main/details-main.jsx
@@ -6,15 +6,24 @@ const DetailsMain = () => {
   return (
     <div className="details__main">
       <div className="details__sky">
-        <img src="images/sunny.svg" alt="" className="details__sky-icon" />
+        <img src="images/sunny.svg" alt="icon" className="details__sky-icon" />
         <h2 className="details__sky-title">Sunny</h2>
       </div>
       <div className="details__temperature">
-        <span className="details__temperature-number">33</span>
+        <span className="details__temperature-number">
+          33
+          <span className="celsius">&#176;C</span>
+        </span>
       </div>
-      <div className="details__interval">
-        <span className="details__interval-max">35C</span>
-        <span className="details__interval-min">27C</span>
+      <div className="details__intervals">
+        <span className="details__interval-max">
+          35
+          <span className="celsius">&#176;C&#8593;</span>
+        </span>
+        <span className="details__interval-min">
+          27
+          <span className="celsius">&#176;C&#8595;</span>
+        </span>
       </div>
     </div>
   );
